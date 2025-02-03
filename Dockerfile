0281c4fbb8cf6be02990677c5c0471d0e32f4118
Abhishek Ghosh <abhishek.ghosh@thoughtworks.com>
2025-02-03 12:09:35 +0530

    [IND-14|Abhishek/Anuja/Sayan] updated Dockerfile, docker-compose.yml and init.sh

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,12 @@ RUN adduser --system --no-create-home --disabled-password --shell /bin/bash user
 
 WORKDIR /opt/ska-src-mm-image-discovery-api
 
+COPY --chown=user requirements.txt requirements.txt
+
+RUN python3 -m pip install -r requirements.txt --extra-index-url https://gitlab.com/api/v4/projects/48060714/packages/pypi/simple
+
 COPY --chown=user . .
 
-RUN python3 -m pip install -e . --extra-index-url https://gitlab.com/api/v4/projects/48060714/packages/pypi/simple
+EXPOSE 8080
 
 ENTRYPOINT ["/bin/bash", "etc/docker/init.sh"]
--- etc/docker/init.sh
+++ etc/docker/init.sh
@@ -9,10 +9,13 @@ env
 
 # set the root path for openapi docs (https://fastapi.tiangolo.com/advanced/behind-a-proxy/)
 # this should match any proxy path redirect
-cmd="src.ska_src_mm_image_discovery_api.rest.server:app --host "0.0.0.0" --port 8080 --reload --reload-dir ../models/ --reload-dir ../client/ --reload-dir ../rest/ --reload-dir ../common/ --reload-dir ../../../etc/ --reload-include *.json"
+cmd="src.ska_src_mm_image_discovery_api.rest.server:app --host "0.0.0.0" --port 8080 --reload --reload-dir src/ska_src_mm_image_discovery_api/ --reload-dir etc/ --reload-include *.json"
+
+# Adding api root path if it is in the environment
 if [ ! -z "API_ROOT_PATH" -a "$API_ROOT_PATH" != "" ]; then
   cmd+=' --root-path '$API_ROOT_PATH
 fi
+
 echo $cmd
 
 echo $cmd | xargs uvicorn
