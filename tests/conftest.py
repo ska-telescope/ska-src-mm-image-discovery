1dd6f5e9e97b0f9d07b836fb2e65b9b8bdf6bb71
Sayan Roy <sayan_roy@Sayans-MacBook-Pro.local>
2025-02-04 11:12:26 +0530

    [IND-14|Abhishek/Anuja/Sayan] add __init__.py in tests/config

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import sys
-from os import path
-
-print("****************************** Adding project_path to sys PATH ******************************")
-project_path = path.dirname(path.dirname(path.abspath(__file__)))
-sys.path.append(f'{project_path}/src')
