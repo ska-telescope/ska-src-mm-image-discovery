d55fd31138e47291fd359d2b15865235136bf64d
Abhishek Ghosh <abhishek.ghosh@thoughtworks.com>
2025-02-18 12:40:28 +0530

    [IND-18|Abhishek/Anuja/Sayan] Added 2 parallel skopeo inspect calls

--- src/ska_src_mm_image_discovery_api/common/skopeo.py
+++ src/ska_src_mm_image_discovery_api/common/skopeo.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import logging
 
@@ -15,8 +16,15 @@ class Skopeo:
         pass
 
     async def inspect(self, image_url: str) -> dict:
+        results = await asyncio.gather(
+            self.__inspect(image_url, "--tls-verify=false --raw"),
+            self.__inspect(image_url, "--tls-verify=false")
+        )
+        return {**results[0], **results[1]}
+
+    async def __inspect(self, image_url: str, options: str) -> dict:
         try:
-            cmd = CommandExecutor(f"skopeo inspect docker://{image_url} --tls-verify=false --raw")
+            cmd = CommandExecutor(f"skopeo inspect docker://{image_url} {options}")
             result = await cmd.execute()
             return json.loads(result)
         except Exception as err:
--- src/ska_src_mm_image_discovery_api/service/metadata_service.py
+++ src/ska_src_mm_image_discovery_api/service/metadata_service.py
@@ -1,6 +1,7 @@
 import base64
 import json
 import logging
+import os
 
 from fastapi import HTTPException
 
@@ -17,6 +18,10 @@ class MetadataService:
     def __init__(self, mongo_repository: MongoRepository, skopeo: Skopeo):
         self.skopeo = skopeo
         self.mongo_repository = mongo_repository
+        self.annotation_key = os.getenv("ANNOTATION_KEY", "annotations")
+        self.metadata_key = os.getenv("METADATA_KEY", "org.opencadc.image-metadata")
+        self.digest_key = os.getenv("DIGEST_KEY", "Digest")
+
 
     async def get_all_metadata(self, metadata_filter: dict) -> list[ImageMetadata]:
         metadata_filter = {"types": metadata_filter.get('type_name')} if metadata_filter.get(
@@ -37,27 +42,27 @@ class MetadataService:
 
     async def register_metadata(self, image_url: str) -> ImageMetadata:
         result = await self.skopeo.inspect(image_url)
-        ANNOTATIONS = "annotations"
-        IMAGE_METADATA = "org.opencadc.image-metadata"
-        DIGEST = "Digest"
+        self.logger.info(f"Image metadata found for image {image_url} is {result}")
 
-        if ANNOTATIONS not in result or IMAGE_METADATA not in result.get(ANNOTATIONS):
+        if self.annotation_key not in result or self.metadata_key not in result.get(self.annotation_key):
             self.logger.error(f"Image metadata not found for image {image_url}")
             raise HTTPException(status_code=404, detail=f"Image metadata not found for image {image_url}")
 
         ## TODO - keys can vary, need to handle this, check json loads is required or not
-        annotations = result.get(ANNOTATIONS)
-        digest = result.get(DIGEST, "DEFAULT_DIGEST")
-        metadata = self.__decode_metadata(annotations.get(IMAGE_METADATA))
+        annotations = result.get(self.annotation_key)
+        digest = result.get(self.digest_key, "DEFAULT_DIGEST")
+        metadata = self.__decode_metadata(annotations.get(self.metadata_key))
 
         ## TODO changes keys and introduce version+tag and name+image_url
-        return ImageMetadata(
+        image_metadata = ImageMetadata(
             image_id=metadata.get("Name"),
             author_name=metadata.get("Author"),
             types=metadata.get("Types"),
             digest=digest,
             tag=metadata.get("Version")
         )
+        return await self.mongo_repository.register_image_metadata(image_metadata)
+
 
     ## TODO - this method can be moved to different class
     @staticmethod
