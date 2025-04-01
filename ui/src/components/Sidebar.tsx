5123dedc8058a1b8d2f526e915cdd4938d3a53a5
Sayan Roy <sayan.roy@thoughtworks.com>
2025-04-01 10:58:17 +0530

    [SP-5279|Abhishek/Anuja/Sayan] Change  Button Style in Sidebar

--- ui/src/components/Sidebar.tsx
+++ ui/src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ import Collapse from "@mui/material/Collapse";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import { useState } from "react";
-import { Button } from "@mui/material";
+// import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export default function Sidebar() {
@@ -23,10 +23,8 @@ export default function Sidebar() {
                     </ListItemButton>
                     <Collapse in={openSoftware} timeout="auto" unmountOnExit>
                         <List component="div" disablePadding>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <Button component={Link} to="/software/search" variant="text" color={"secondary"}>
-                                    GET Software Metadata
-                                </Button>
+                            <ListItemButton component={Link} to="/software/search" sx={{ pl: 4  , color:"#E5096A" }}>
+                                GET Software Metadata
                             </ListItemButton>
                         </List>
                     </Collapse>
@@ -38,15 +36,11 @@ export default function Sidebar() {
                     </ListItemButton>
                     <Collapse in={openImage} timeout="auto" unmountOnExit>
                         <List component="div" disablePadding>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <Button component={Link} to="/image-discovery/id" variant="text" color={"secondary"}>
-                                    GET Image by ID
-                                </Button>
+                            <ListItemButton component={Link} to="/image-discovery/id" sx={{ pl: 4 , color:"#E5096A"}}>
+                                GET Image by ID
                             </ListItemButton>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <Button component={Link} to="/image-discovery/type" variant="text" color={"secondary"}>
-                                    GET Image by Type
-                                </Button>
+                            <ListItemButton component={Link} to="/image-discovery/type" sx={{ pl: 4 , color:"#E5096A" }}>
+                                GET Image by Type
                             </ListItemButton>
                         </List>
                     </Collapse>
